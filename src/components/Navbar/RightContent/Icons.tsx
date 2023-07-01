eb78dc989c522c95741472ed84564317edfb1794
Jeff Jackson <105036000+jeffRnR@users.noreply.github.com>
2023-07-01 16:18:20 +0300

    searh button

--- src/components/Navbar/RightContent/Icons.tsx
+++ src/components/Navbar/RightContent/Icons.tsx
@@ -1,108 +1,65 @@
-import React, { useState } from 'react';
-import { AddIcon } from "@chakra-ui/icons";
-import { Box, Flex, Icon } from "@chakra-ui/react";
-import { BsArrowUpRightCircle, BsChatDots } from "react-icons/bs";
-import { GrAdd } from "react-icons/gr";
-import { HiOutlineShoppingBag } from "react-icons/hi";
-import { HiOutlineMapPin } from "react-icons/hi2";
-import {
-  IoFilterCircleOutline,
-  IoNotificationsOutline,
-  IoVideocamOutline,
-} from "react-icons/io5";
+import React from 'react';
+import { AddIcon } from '@chakra-ui/icons';
+import { Flex, Icon } from '@chakra-ui/react';
+import { HiOutlineMap, HiOutlineShoppingBag, HiOutlineUser } from 'react-icons/hi';
+import { IoNotificationsOutline } from 'react-icons/io5';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-
-const products = [];
 const Icons: React.FC = () => {
   const router = useRouter();
 
-  const [cartVisibility, setCartVisibility] = useState(false);
-  const [productsInCart, setProducts] = useState([])
-
   return (
     <Flex>
+      <Link href="/Map/Map">
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: 'gray.200' }}
+        >
+          <Icon as={HiOutlineMap} fontSize={22} />
+        </Flex>
+      </Link>
+
+      <Link href="/src/components/Cart/Cart"> {/* Update the link to the cart file */}
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: 'gray.200' }}
+        >
+          <Icon as={HiOutlineShoppingBag} fontSize={20} />
+        </Flex>
+      </Link>
+
       <Flex
-        display={{ base: 'none', md: 'flex'}}
-        align='center'
-        borderRight='1px solid'
-        borderColor='blue.200'
+        mr={1.5}
+        ml={1.5}
+        padding={1}
+        cursor="pointer"
+        borderRadius={4}
+        _hover={{ bg: 'gray.200' }}
       >
-        {/*<Flex 
-            mr={1.5} 
-            ml={1.5} 
-            padding={1} 
-            cursor='pointer' 
-            borderRadius={4} 
-            _hover={{ bg:'gray.200'}}                    
-        >
-            <Icon as={HiOutlineMapPin} fontSize={22}/>
-        </Flex>
-        
-        <Flex 
-            mr={1.5} 
-            ml={1.5} 
-            padding={1} 
-            cursor='pointer' 
-            borderRadius={4} 
-            _hover={{ bg:'gray.200'}}                    
-        >
-            <Icon as={HiOutlineShoppingBag} fontSize={20}/>
-        </Flex> */}
+        <Icon as={IoNotificationsOutline} fontSize={20} />
       </Flex>
-      <>
-        <Link href='/Map/Map'>
-          <Flex
-            mr={1.5}
-            ml={1.5}
-            padding={1}
-            cursor='pointer'
-            borderRadius={4}
-            _hover={{ bg:'gray.200'}}
-          >
-            <Icon as={HiOutlineMapPin} fontSize={22}/>
-          </Flex>
-        </Link>
-
-        <Link href='/src/components/Cart/Cart'> {/* Update the link to the cart file */}
-          <Flex
-            mr={1.5}
-            ml={1.5}
-            padding={1}
-            cursor='pointer'
-            borderRadius={4}
-            _hover={{ bg:'gray.200'}}
-            
-          >
-            <Icon as={HiOutlineShoppingBag} fontSize={20}/>
-          </Flex>
-        </Link>
-
-        <Flex
-          mr={1.5}
-          ml={1.5}
-          padding={1}
-          cursor='pointer'
-          borderRadius={4}
-          _hover={{ bg:'gray.200'}}
-        >
-          <Icon as={IoNotificationsOutline} fontSize={20}/>
-        </Flex>
-        <Flex
-          display={{ base:'none', md: 'flex'}}
-          mr={1.5}
-          ml={1.5}
-          padding={1}
-          cursor='pointer'
-          borderRadius={4}
-          _hover={{ bg:'gray.200'}}
-        >
-          <Icon as={GrAdd} fontSize={20}/>
-        </Flex>
-      </>
+      <Flex
+        display={{ base: 'none', md: 'flex' }}
+        mr={1.5}
+        ml={1.5}
+        padding={1}
+        cursor="pointer"
+        borderRadius={4}
+        _hover={{ bg: 'gray.200' }}
+      >
+        <Icon as={HiOutlineUser} fontSize={20} />
+      </Flex>
     </Flex>
   );
-}
+};
 
 export default Icons;
--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,41 +1,39 @@
 import { SearchIcon } from '@chakra-ui/icons';
-import { Flex, Input, InputGroup, InputLeftElement, Stack, border } from '@chakra-ui/react';
+import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { User } from '@firebase/auth';
 import React from 'react';
 
 type SearchInputProps = {
-    user?: User | null;
+  user?: User | null;
 };
 
-const SearchInput:React.FC<SearchInputProps> = ({ user }) => {
-    
-    return (
-        <Flex align='center' flexGrow={1} maxWidth={user ? 'auto' : '600px'} marginLeft='10px' mr={2}>            
-            <InputGroup>
-                <InputLeftElement pointerEvents='none'>
-                    <SearchIcon color='gray.300' mb={1} />
-                    
-                </InputLeftElement>
-                <Input 
-                    placeholder='Search omp' 
-                    fontSize="12px" 
-                    _placeholder={{ color:"gray:500"}}
-                    _hover={{
-                        bg: 'white',
-                        border: '1px solid',
-                        borderColor: 'gray.500'
-                    }} 
-                    _focus={{
-                        outline: 'none',
-                        border: '1px solid',
-                        borderColor: "blue.500",
-                    }}
-                    height='34px'
-                    bg='gray.50'
-                />
-            </InputGroup>
-            
-        </Flex>
-    )
-}
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
+  return (
+    <Flex align="center" flexGrow={1} maxWidth={user ? 'auto' : '600px'} marginLeft="10px" mr={2}>
+      <InputGroup>
+        <InputLeftElement pointerEvents="none">
+          <SearchIcon color="gray.300" mb={1} />
+        </InputLeftElement>
+        <Input
+          placeholder="Search omp"
+          fontSize="12px"
+          _placeholder={{ color: 'gray:500' }}
+          _hover={{
+            bg: 'white',
+            border: '1px solid',
+            borderColor: 'gray.500',
+          }}
+          _focus={{
+            outline: 'none',
+            border: '1px solid',
+            borderColor: 'blue.500',
+          }}
+          height="34px"
+          bg="gray.50"
+        />
+      </InputGroup>
+    </Flex>
+  );
+};
+
 export default SearchInput;
